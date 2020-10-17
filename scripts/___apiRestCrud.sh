cf5639c8febfc4866eaa1772ba46517ff4e64a5a
Alejandro Martínez <amartinezcerro90@gmail.com>
2020-10-17 11:28:17 +0200

    ARC-4 Added ___apiRestCrud.sh script to manage the project

---
+++ scripts/___apiRestCrud.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+echo "\nWhat do you want to do?"
+echo "1. Remove docker containers"
+echo "2. Start docker containers"
+echo "3. Compile"
+echo "4. Start the server"
+echo "5. Compile and start the server"
+echo "0. Exit"
+
+PROJECT_PATH=~/personal/api-rest-crud/
+
+while :
+do
+	read OPTION
+	echo "\nYou have selected:" $OPTION
+	case $OPTION in
+		0)
+			echo "Disconnected\n"
+			break
+			;;
+		1)
+			echo "Removing docker containers..."
+			docker rm arc-mysql -f
+			break
+			;;
+		2)
+			echo "Starting docker containers..."
+			cd $PROJECT_PATH/docker/mysql && docker-compose up -d
+			break
+			;;
+		3)
+			echo "Compiling..."
+			cd $PROJECT_PATH && ./gradlew classes && ./gradlew testClasses
+			break
+			;;
+		4)
+			echo "Starting the server..."
+			cd $PROJECT_PATH && ./gradlew bootRun
+			break
+			;;
+		5)
+			echo "Compiling and starting the server..."
+			cd $PROJECT_PATH && ./gradlew classes && ./gradlew testClasses && ./gradlew bootRun
+			break
+			;;
+		*)
+			echo "Select a correct option"
+			;;    
+  esac
+done
