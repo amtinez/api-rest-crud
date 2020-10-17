bb1ddddf5c57b16d128daac88d48d711c99d9f8c
Alejandro Martínez <amartinezcerro90@gmail.com>
2020-10-17 11:15:41 +0200

    ARC-1 Created the project

---
+++ src/test/java/com/amtinez/api/rest/crud/ApiRestCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amtinez.api.rest.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
