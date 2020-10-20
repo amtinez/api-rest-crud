ec54c0b4603e11d48a4c6cdee01154a5d4552379
Alejandro Martínez <amartinezcerro90@gmail.com>
2020-10-20 22:51:18 +0200

    ARC-10 Added models, constants and modified create-tables.sql

---
+++ src/main/java/com/amtinez/api/rest/crud/models/AuthorityModel.java
@@ -0,0 +1,48 @@
+package com.amtinez.api.rest.crud.models;
+
+import com.amtinez.api.rest.crud.constants.DatabaseConstants.Table.Authority;
+import com.amtinez.api.rest.crud.constants.DatabaseConstants.Table.UsersAuthorities;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import static com.amtinez.api.rest.crud.constants.DatabaseConstants.DATABASE_NAME;
+
+/**
+ * @author amartinezcerro@gmail.com
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = Authority.TABLE_NAME,
+       catalog = DATABASE_NAME)
+public class AuthorityModel implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = Authority.AUTHORITY_FIELD, length = Authority.AUTHORITY_FIELD_LENGTH)
+    private String authority;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = UsersAuthorities.TABLE_NAME, catalog = DATABASE_NAME, joinColumns = {
+        @JoinColumn(name = UsersAuthorities.ID_AUTHORITY_FIELD, nullable = false, updatable = false)}, inverseJoinColumns = {
+        @JoinColumn(name = UsersAuthorities.ID_USER_FIELD, nullable = false, updatable = false)})
+    private Set<UserModel> users = new HashSet<>(0);
+
+}
