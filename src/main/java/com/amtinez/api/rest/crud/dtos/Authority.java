0ac0dec2a6af54895eb336a3c96a5e7094ab4259
Alejandro Martínez <amartinezcerro90@gmail.com>
2020-10-24 14:11:31 +0200

    ARC-12 Renamed data classes

---
+++ src/main/java/com/amtinez/api/rest/crud/dtos/Authority.java
@@ -0,0 +1,14 @@
+package com.amtinez.api.rest.crud.dtos;
+
+import lombok.Data;
+
+/**
+ * @author amartinezcerro@gmail.com
+ */
+@Data
+public class Authority {
+
+    private Long id;
+    private String name;
+
+}
