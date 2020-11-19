8f1c8c8143fd5dbe1651b54d6ad875fe2563965e
Alejandro Martínez <amartinezcerro90@gmail.com>
2020-11-19 23:31:23 +0100

    ARC-13 Added and modified constants

--- src/main/java/com/amtinez/api/rest/crud/constants/SecurityConstants.java
+++ src/main/java/com/amtinez/api/rest/crud/constants/SecurityConstants.java
@@ -8,6 +8,8 @@ public class SecurityConstants {
     public static final int PASSWORD_LIFETIME_MONTHS = 6;
     public static final int INACTIVE_LIFETIME_MONTHS = 3;
 
+    public static final String USER_EMAIL_NOT_FOUND = "User with the email: %s not found";
+
     private SecurityConstants() {
 
     }
