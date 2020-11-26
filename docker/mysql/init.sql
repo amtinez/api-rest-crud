5fb7a0edafe0d525df9f375bebed44c7336b20af
Alejandro Martínez <amartinezcerro90@gmail.com>
2020-11-26 22:32:12 +0100

    ARC-25 Added locked_at and removed deleted_by and deleted_at fields

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -12,11 +12,10 @@ CREATE TABLE IF NOT EXISTS `users`
     `created_at`              TIMESTAMP    NOT NULL,
     `last_updated_by`         VARCHAR(100) NOT NULL,
     `last_updated_at`         TIMESTAMP    NOT NULL,
-    `deleted_by`              VARCHAR(100) DEFAULT NULL,
-    `deleted_at`              TIMESTAMP    DEFAULT NULL,
-    `last_access_at`          TIMESTAMP    DEFAULT NULL,
-    `last_password_update_at` TIMESTAMP    DEFAULT NULL,
-    `enabled`                 BOOLEAN      DEFAULT NULL,
+    `locked_at`               TIMESTAMP DEFAULT NULL,
+    `last_access_at`          TIMESTAMP DEFAULT NULL,
+    `last_password_update_at` TIMESTAMP DEFAULT NULL,
+    `enabled`                 BOOLEAN   DEFAULT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `UK_email` (`email`)
 ) ENGINE = InnoDB
@@ -31,8 +30,6 @@ CREATE TABLE IF NOT EXISTS `authorities`
     `created_at`      TIMESTAMP    NOT NULL,
     `last_updated_by` VARCHAR(100) NOT NULL,
     `last_updated_at` TIMESTAMP    NOT NULL,
-    `deleted_by`      VARCHAR(100) DEFAULT NULL,
-    `deleted_at`      TIMESTAMP    DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
