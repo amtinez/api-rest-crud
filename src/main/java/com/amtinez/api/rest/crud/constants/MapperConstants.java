1566426a965d53aeda0108b6aad4dbe9bd0bec4b
Alejandro Martínez <amartinezcerro90@gmail.com>
2020-11-26 23:55:26 +0100

    ARC-21 Added more MapperConstants

--- src/main/java/com/amtinez/api/rest/crud/constants/MapperConstants.java
+++ src/main/java/com/amtinez/api/rest/crud/constants/MapperConstants.java
@@ -10,8 +10,23 @@ public class MapperConstants {
     public static class User {
 
         public static final String PASSWORD_PROPERTY = "password";
+        public static final String USERS_PROPERTY = "users";
+
+        public static final String AUTHORITY_MODEL_TO_AUTHORITY_WITHOUT_USERS = "authorityModelToAuthorityWithoutUsers";
+        public static final String AUTHORITY_TO_AUTHORITY_MODEL_WITHOUT_USERS = "authorityToAuthorityModelWithoutUsers";
 
         private User() {
+        }
+    }
+
+    public static class Authority {
+
+        public static final String AUTHORITIES_PROPERTY = "authorities";
+
+        public static final String USER_MODEL_TO_USER_WITHOUT_AUTHORITIES = "userModelToUserWithoutAuthorities";
+        public static final String USER_TO_USER_MODEL_WITHOUT_AUTHORITIES = "userToUserModelWithoutAuthorities";
+
+        private Authority() {
         }
     }
 
