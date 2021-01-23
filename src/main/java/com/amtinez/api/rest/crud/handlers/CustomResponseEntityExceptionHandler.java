d77da252138e332c5c1fdd97049458fa681ace31
Alejandro Martínez <amartinezcerro90@gmail.com>
2021-01-24 00:17:45 +0100

    ARC-33 Added CustomResponseEntityExceptionHandler to handle ArgumentNotValid

---
+++ src/main/java/com/amtinez/api/rest/crud/handlers/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.amtinez.api.rest.crud.handlers;
+
+import com.amtinez.api.rest.crud.errors.FieldError;
+import com.amtinez.api.rest.crud.errors.ValidationError;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author amartinezcerro@gmail.com
+ */
+@ControllerAdvice
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
+                                                               final HttpHeaders headers,
+                                                               final HttpStatus status,
+                                                               final WebRequest request) {
+        final List<FieldError> errors = ex.getBindingResult()
+                                          .getFieldErrors()
+                                          .stream()
+                                          .map(fieldError -> FieldError.builder()
+                                                                       .field(fieldError.getField())
+                                                                       .message(fieldError.getDefaultMessage())
+                                                                       .build())
+                                          .collect(Collectors.toList());
+        final ValidationError validationError = ValidationError.builder()
+                                                               .errors(errors)
+                                                               .build();
+        return new ResponseEntity<>(validationError, headers, status);
+    }
+
+}
