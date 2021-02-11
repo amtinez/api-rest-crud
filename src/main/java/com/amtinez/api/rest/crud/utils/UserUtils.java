bd149b26a8f5d7af799b3116a138461274bb1423
Alejandro Martínez <amartinezcerro90@gmail.com>
2021-02-11 22:36:00 +0100

    ARC-28 Added javadoc to UserUtils

--- src/main/java/com/amtinez/api/rest/crud/utils/UserUtils.java
+++ src/main/java/com/amtinez/api/rest/crud/utils/UserUtils.java
@@ -7,6 +7,12 @@ import com.amtinez.api.rest.crud.models.UserModel;
  */
 public class UserUtils {
 
+    /**
+     * Returns the full name of the user
+     *
+     * @param userModel the user
+     * @return the full name of the user
+     */
     public static String getFullName(final UserModel userModel) {
         return String.format("%s %s", userModel.getFirstName(), userModel.getLastName());
     }
