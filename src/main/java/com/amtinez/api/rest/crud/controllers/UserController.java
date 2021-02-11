bac9c46d6cdf10c51cdbda588d8a9248886893f1
Alejandro Martínez <amartinezcerro90@gmail.com>
2021-02-11 22:41:22 +0100

    ARC-28 Modified the mapping for the registerUser method and added more methods in UserController for user control

--- src/main/java/com/amtinez/api/rest/crud/controllers/UserController.java
+++ src/main/java/com/amtinez/api/rest/crud/controllers/UserController.java
@@ -2,13 +2,16 @@ package com.amtinez.api.rest.crud.controllers;
 
 import com.amtinez.api.rest.crud.dtos.User;
 import com.amtinez.api.rest.crud.facades.UserFacade;
+import com.amtinez.api.rest.crud.utils.ResponseEntityUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,7 +35,7 @@ public class UserController {
         return ResponseEntity.ok(userFacade.findAllUsers());
     }
 
-    @PutMapping
+    @PostMapping
     public ResponseEntity<User> registerUser(@Valid @RequestBody final User user) {
         return ResponseEntity.ok(userFacade.registerUser(user));
     }
@@ -52,6 +55,26 @@ public class UserController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PatchMapping("/{id}/enable")
+    public ResponseEntity<Void> enableUser(@PathVariable final Long id) {
+        return ResponseEntityUtils.getResponseEntityByAffectedEntities(userFacade.enableUser(id));
+    }
+
+    @PatchMapping("/{id}/disable")
+    public ResponseEntity<Void> disableUser(@PathVariable final Long id) {
+        return ResponseEntityUtils.getResponseEntityByAffectedEntities(userFacade.disableUser(id));
+    }
+
+    @PatchMapping("/{id}/lock")
+    public ResponseEntity<Void> lockUser(@PathVariable final Long id, @RequestParam final String reason) {
+        return ResponseEntityUtils.getResponseEntityByAffectedEntities(userFacade.lockUser(id, reason));
+    }
+
+    @PatchMapping("/{id}/unlock")
+    public ResponseEntity<Void> unlockUser(@PathVariable final Long id) {
+        return ResponseEntityUtils.getResponseEntityByAffectedEntities(userFacade.unlockUser(id));
     }
 
 }
