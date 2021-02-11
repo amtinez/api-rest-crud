a5138aaa52604bf9703a62fb905b9075d6033621
Alejandro Martínez <amartinezcerro90@gmail.com>
2021-02-11 22:37:20 +0100

    ARC-28 Added two new methods in UserDao to update the user

--- src/main/java/com/amtinez/api/rest/crud/daos/UserDao.java
+++ src/main/java/com/amtinez/api/rest/crud/daos/UserDao.java
@@ -2,6 +2,12 @@ package com.amtinez.api.rest.crud.daos;
 
 import com.amtinez.api.rest.crud.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 /**
  * @author amartinezcerro@gmail.com
@@ -23,5 +29,34 @@ public interface UserDao extends JpaRepository<UserModel, Long> {
      * @return if email address exists
      */
     boolean existsByEmail(final String email);
+
+    /**
+     * Enables or disables the user if it exists
+     *
+     * @param id      the id of the user
+     * @param enabled the enabled of the user
+     * @return number of updated users
+     */
+    @Modifying
+    @Transactional
+    @Query("UPDATE UserModel AS U SET U.enabled = :enabled WHERE U.id = :id")
+    int updateEnabledStatusById(@Param("id") final Long id, @Param("enabled") final Boolean enabled);
+
+    /**
+     * Lock or unlock the user if it exists
+     *
+     * @param id           the id of the user
+     * @param lockedBy     the full name of the lock executor
+     * @param lockedDate   the date of the lock
+     * @param lockedReason the reason of the lock
+     * @return number of updated users
+     */
+    @Modifying
+    @Transactional
+    @Query("UPDATE UserModel AS U SET U.lockedBy = :lockedBy, U.lockedDate = :lockedDate, U.lockedReason = :lockedReason WHERE U.id = :id")
+    int updateLockedInformationById(@Param("id") final Long id,
+                                    @Param("lockedBy") final String lockedBy,
+                                    @Param("lockedDate") final LocalDateTime lockedDate,
+                                    @Param("lockedReason") final String lockedReason);
 
 }
